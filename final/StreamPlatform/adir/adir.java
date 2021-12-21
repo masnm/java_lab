023b1b46dc5cf65bd15db3cb423ef1b76834858f
Ahmmed Sakib Noman <nomanahmmed0@gmail.com>
2021-12-21 12:07:45 +0600

    Cleaned Up and Closed

--- final/StreamPlatform/adir/adir.java
+++ final/StreamPlatform/adir/adir.java
@@ -3,6 +3,6 @@ package StreamPlatform.adir;
 public class adir {
 	public void sayHi () {
 		System.out.println ( "Finally saying hii form adir" );
-		System.out.println ( "Hope i didn't mess up this time" );
+		System.out.println ( "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nHope i didn't mess up this time" );
 	}
 }
